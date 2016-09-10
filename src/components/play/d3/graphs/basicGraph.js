44f917a90f6000293abb85e8a355003b41e2c4ff
Andrew Zures <andrew.zures@gmail.com>
2016-09-10 18:02:13 -0500

    thru bar chart v2

--- src/components/play/d3/graphs/basicGraph.js
+++ src/components/play/d3/graphs/basicGraph.js
@@ -13,24 +13,38 @@ export default class BasicGraph extends Component {
 
   renderChart() {
     let data = [4, 8, 15, 16, 23, 42];
+    let width = 420;
+    let barHeight = 20;
 
     let x = d3.scale.linear()
     .domain([0, d3.max(data)])
-    .range([0, 800]);
+    .range([0, width]);
 
-    d3.select('#basic-chart')
-    .selectAll("div")
-      .data(data)
-    .enter().append("div")
-      .style("width", d => x(d) + "px")
-      .text(d => d);
+    let chart = d3.select('#basic-chart')
+    .attr('width', width)
+    .attr('height', barHeight * data.length);
+
+    let bar = chart
+    .selectAll('g').data(data)
+    .enter().append('g')
+    .attr('transform', (d, i) => `translate(0,${i*barHeight})`);
+
+    bar.append('rect')
+    .attr('width', x)
+    .attr('height', barHeight - 1);
+
+    bar.append('text')
+    .attr('x', d => x(d) - 3)
+    .attr('y', barHeight / 2)
+    .attr('dy', '.35em')
+    .text(d => d)
   }
 
   render() {
     return (
       <div>
         <h1>BasicGraph</h1>
-        <div id="basic-chart" className="chart" />
+        <svg id="basic-chart" className="chart" />
       </div>
     );
   }
