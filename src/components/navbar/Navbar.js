0fd679ec834ca3b7e1cb0c7cfa7edd8723ee1cf2
Andrew Zures <andrew.zures@gmail.com>
2016-09-25 13:15:07 -0500

    adds navigation styling

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,11 +4,16 @@ import { Link } from 'react-router';
 export default class NavBar extends Component {
   render() {
     return (
-      <div>
-        <ul>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/play">Play</Link></li>
-        </ul>
+      <div className="main-content">
+        <header>
+          <div className="header-title">
+            <h2>Ixora</h2>
+          </div>
+          <ul className="navbar-list">
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/play">Play</Link></li>
+          </ul>
+        </header>
         {this.props.children}
       </div>
     );
--- src/components/play/Play.js
+++ src/components/play/Play.js
@@ -5,10 +5,12 @@ export default class Play extends Component {
   render() {
     return (
       <div>
-        <ul>
-          <li><Link to="/play/d3/bar_chart">Bar Chart</Link></li>
-          <li><Link to="/play/d3/three_circles">Three Circles</Link></li>
-        </ul>
+        <div className="navbar-menu">
+          <ul>
+            <li><Link to="/play/d3/bar_chart">Bar Chart</Link></li>
+            <li><Link to="/play/d3/three_circles">Three Circles</Link></li>
+          </ul>
+        </div>
         {this.props.children}
       </div>
     );
